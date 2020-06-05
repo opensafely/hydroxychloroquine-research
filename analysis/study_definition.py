32ff7ce05127c174bf6a4000451b01437d0c71a2
Brian <brianmkcebm@gmail.com>
2020-06-05 13:40:15 +0100

    commit first batch of clinical codes
    more to be done but calls for the afternoon

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -139,5 +139,127 @@ study = StudyDefinition(
         },
     ),
 
+    #CLINICAL COVARIATES
+    #BMI
+    bmi=patients.most_recent_bmi(
+        on_or_after="2010-02-01",
+        minimum_age_at_measurement=16,
+        include_measurement_date=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.6,
+            "float": {"distribution": "normal", "mean": 35, "stddev": 10},
+        },
+    ),
+    #SMOKING
+    smoking_status=patients.categorised_as(
+        {
+            "S": "most_recent_smoking_code = 'S'",
+            "E": """
+                     most_recent_smoking_code = 'E' OR (    
+                       most_recent_smoking_code = 'N' AND ever_smoked   
+                     )  
+                """,
+            "N": "most_recent_smoking_code = 'N' AND NOT ever_smoked",
+            "M": "DEFAULT",
+        },
+        return_expectations={
+            "category": {"ratios": {"S": 0.6, "E": 0.1, "N": 0.2, "M": 0.1}}
+        },
+        most_recent_smoking_code=patients.with_these_clinical_events(
+            clear_smoking_codes,
+            find_last_match_in_period=True,
+            on_or_before="2020-02-29",
+            returning="category",
+        ),
+        ever_smoked=patients.with_these_clinical_events(
+            filter_codes_by_category(clear_smoking_codes, include=["S", "E"]),
+            on_or_before="2020-02-29",
+        ),
+    ),
+    smoking_status_date=patients.with_these_clinical_events(
+        clear_smoking_codes,
+        on_or_before="2020-02-29",
+        return_last_date_in_period=True,
+        include_month=True,
+        return_expectations={"date": {"latest": "2020-02-29"}},
+    ),
+
+
+    #HYPERTENSION - CLINICAL CODES ONLY
+    hypertension=patients.with_these_clinical_events(
+        hypertension_codes,
+        on_or_before="2020-02-29",
+        return_first_date_in_period=True,
+        include_month=True,
+        return_expectations={"date": {"latest": "2020-02-29"}},
+    ),
+
+    chronic_cardiac_disease=patients.with_these_clinical_events(
+        chronic_cardiac_disease_codes,
+        on_or_before="2020-02-29",
+        return_first_date_in_period=True,
+        include_month=True,
+        return_expectations={"date": {"latest": "2020-02-29"}},
+        ),
+
+    diabetes=patients.with_these_clinical_events( #check my understanding I think we need this to detect unmeasured HbA1C
+        diabetes_codes,
+        on_or_before="2020-02-29",
+        return_first_date_in_period=True,
+        include_month=True,
+        return_expectations={"date": {"latest": "2020-02-29"}},
+    ),
+
+       hba1c_mmol_per_mol=patients.with_these_clinical_events(
+        hba1c_new_codes,
+        find_last_match_in_period=True,
+        on_or_before="2020-02-29",
+        returning="numeric_value",
+        include_date_of_match=True,
+        include_month=True,
+        return_expectations={
+            "date": {"latest": "2020-02-29"},
+            "float": {"distribution": "normal", "mean": 40.0, "stddev": 20},
+            "incidence": 0.95,
+        },
+    ),
+
+    hba1c_percentage=patients.with_these_clinical_events(
+        hba1c_old_codes,
+        find_last_match_in_period=True,
+        on_or_before="2020-02-29",
+        returning="numeric_value",
+        include_date_of_match=True,
+        include_month=True,
+        return_expectations={
+            "date": {"latest": "2020-02-29"},
+            "float": {"distribution": "normal", "mean": 5, "stddev": 2},
+            "incidence": 0.95,
+        },
+    ),
+
+    #CHRONIC RESPIRATORY DISEASES - EXCL ASTHMA
+    chronic_respiratory_excl_asthma=patients.with_these_clinical_events(
+        chronic_respiratory_diasease_codes,
+        on_or_before="2020-02-29",
+        return_first_date_in_period=True,
+        include_month=True,
+        return_expectations={"date": {"latest": "2020-02-29"}},
+    ),
+    
+    #CURRENT ASTHMA
+    current_asthma=patients.with_these_clinical_events(
+        current_asthma_codes,
+        on_or_before="2020-02-29",
+        return_first_date_in_period=True,
+        include_month=True,
+        return_expectations={"date": {"latest": "2020-02-29"}},
+    ),
+
+
+
+
+
 
     
